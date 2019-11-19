49466f7825d968a1459c7ae5ec830f8136d54d56
prasad <prasad.thangavel@gmail.com>
2019-11-19 19:26:23 +0530

    commit 19th nov 2019 -- optimized code

--- app/src/main/java/com/prasad/moviedb/ui/binding/RecyclerViewBinding.kt
+++ app/src/main/java/com/prasad/moviedb/ui/binding/RecyclerViewBinding.kt
@@ -1,17 +1,9 @@
 package com.prasad.moviedb.ui.binding
 
-import android.widget.LinearLayout.HORIZONTAL
-import android.widget.LinearLayout.VERTICAL
-import androidx.databinding.BindingAdapter
-import androidx.recyclerview.widget.DividerItemDecoration
-import androidx.recyclerview.widget.RecyclerView
-import com.prasad.moviedb.data.local.enitity.DiscoverEntity
-import com.prasad.moviedb.ui.adapter.DiscoverAdapter
-
 
 object RecyclerViewBinding {
 
-    @BindingAdapter("app:addVerticalItemDecoration")
+/*    @BindingAdapter("app:addVerticalItemDecoration")
     @JvmStatic
     fun RecyclerView.addVerticalItemDecoration(isVertical: Boolean) {
         addItemDecoration(DividerItemDecoration(context, if (isVertical) VERTICAL else HORIZONTAL))
@@ -23,7 +15,7 @@ object RecyclerViewBinding {
         with(recyclerView.adapter as DiscoverAdapter?) {
             this?.setData(items)
         }
-    }
+    }*/
 /*    @BindingAdapter("app:imageUrl")
     @JvmStatic
     fun loadImage(view: ImageView, imageUrl: String) {
